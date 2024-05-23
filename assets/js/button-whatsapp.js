63f241de28a4ea2eec080db72892b97ab339e906
maelalves <alves.frontend@gmail.com>
2024-05-23 11:10:20 -0300

    versao desktop

---
+++ assets/js/button-whatsapp.js
@@ -0,0 +1,7 @@
+// btn whatsapp
+
+document.getElementById('whatsapp-button').addEventListener('click', function() {
+    // Substitua 'seu_numero' pelo número de telefone com o código do país
+    window.open('http://wa.me/11975703311', '_blank');
+  });
+  
