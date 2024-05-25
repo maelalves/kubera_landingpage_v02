b87936af4b2158f0de3ef1048ca9a08d638325e2
maelalves <alves.frontend@gmail.com>
2024-05-25 19:11:06 -0300

    Lincagem dos btn orcamento com o js

--- assets/js/button-whatsapp.js
+++ assets/js/button-whatsapp.js
@@ -1,7 +1,32 @@
 // btn whatsapp
 
-document.getElementById('whatsapp-button').addEventListener('click', function() {
-    // Substitua 'seu_numero' pelo número de telefone com o código do país
+document.getElementById('whatsapp-button-header').addEventListener('click', function () {
+  // Substitua 'seu_numero' pelo número de telefone com o código do país
+  window.open('http://wa.me/11975703311', '_blank');
+});
+
+
+document.getElementById('whatsapp-button-float').addEventListener('click', function () {
     window.open('http://wa.me/11975703311', '_blank');
-  });
+});
   
+document.getElementById('whatsapp-button-orcamento-footer').addEventListener('click', function () {
+  window.open('http://wa.me/11975703311', '_blank');
+});
+  
+// btn orcamento nichos
+document.getElementById('whatsapp-btn-orcamento1').addEventListener('click', function () {
+  window.open('http://wa.me/11975703311', '_blank');
+});
+
+document.getElementById('whatsapp-btn-orcamento2').addEventListener('click', function () {
+  window.open('http://wa.me/11975703311', '_blank');
+});
+
+document.getElementById('whatsapp-btn-orcamento3').addEventListener('click', function () {
+  window.open('http://wa.me/11975703311', '_blank');
+});
+
+document.getElementById('whatsapp-btn-orcamento4').addEventListener('click', function () {
+  window.open('http://wa.me/11975703311', '_blank');
+});
